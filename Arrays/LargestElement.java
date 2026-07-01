1c940cda940e74f1755bdc1ae8e92f5358e5d2f2
Shrutii Pandey <shrutiipandey16@gmail.com>
2026-07-01 21:06:18 +0530

    Added Largest Element solution

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,1 +1,17 @@
+package Arrays;
 
+public class LargestElement {
+    public static void main(String[]args){
+    int arr[]={5,6,2,9,8};
+    int larg=Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]>larg){
+            larg=arr[i];
+        }
+    }
+     System.out.println("largest element is: " + larg);
+    
+    
+}
+
+}
